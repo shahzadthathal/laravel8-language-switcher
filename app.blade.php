74c66f3263ebafe9bc17b665d21e96fc5c453650
Muhammad Shahzad <shahzadthathal@gmail.com>
2021-09-28 15:38:35 +0400

    Update app.blade.php

--- app.blade.php
+++ app.blade.php
@@ -1,7 +1,8 @@
 @php $locale = session()->get('locale'); @endphp
-							
-							@if(isset($languages))
-								@foreach($languages as $lang)	
-									<a class="dropdown-item" href="{{route('LangChange', 'lang_key='.strtolower($lang->lang_code) )}}">{{$lang->name}}</a>
-								@endforeach
-							@endif
+@php $languagesArr =  [ array('name'=>'English', 'lang_code'=>'en'), array('name'=>'English', 'lang_code'=>'en') ]; @endphp
+
+@foreach($languagesArr as $lang)	
+	<a class="dropdown-item" href="{{route('LangChange', 'lang_key='.strtolower($lang['lang_code']) )}}">{{$lang['name']}}</a>
+@endforeach
+
+						
