6e17b1241972d3684a5b224c33ee07cbd9bfe8c8
Muhammad Shahzad <shahzadthathal@gmail.com>
2021-09-28 14:58:52 +0400

    Create LocalizationController.php

---
+++ LocalizationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+
+class LocalizationController extends Controller
+{
+    public $allowedLangArr = ['en','cn'];
+
+    public function lang_change(Request $request)
+    {
+        $lang_key = $request->lang_key;
+        if(in_array($lang_key, $this->allowedLangArr)){
+            App::setLocale($lang_key);
+            session()->put('locale', $lang_key);
+        }
+        return redirect(app()->getLocale());
+    }
+}
